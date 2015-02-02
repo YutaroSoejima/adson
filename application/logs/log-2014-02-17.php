6af68bfe4f21b3932b4bd551b36c88e12ca88115
yutaro <yutarosoejima54@gmail.com>
2015-02-02 20:19:48 +0900

    delete logs

--- application/logs/log-2014-02-17.php
+++
@@ -1,9 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
-
-DEBUG - 2014-02-17 04:50:40 --> Config Class Initialized
-DEBUG - 2014-02-17 04:50:40 --> Hooks Class Initialized
-DEBUG - 2014-02-17 04:50:40 --> Utf8 Class Initialized
-DEBUG - 2014-02-17 04:50:40 --> UTF-8 Support Enabled
-DEBUG - 2014-02-17 04:50:40 --> URI Class Initialized
-DEBUG - 2014-02-17 04:50:41 --> Router Class Initialized
-ERROR - 2014-02-17 04:50:41 --> 404 Page Not Found --> model_of_sns
